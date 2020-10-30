57a53cbe00b1eb3c2b326826f848347e41a7e4f5
Sameed <sameedulhassan@onebyte.biz>
2020-10-30 11:13:37 +0500

    created author component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,50 +1,55 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import ApolloClient from 'apollo-boost';
-import VueApollo from 'vue-apollo';
-import './bootstrap';
+import ApolloClient from "apollo-boost";
+import VueApollo from "vue-apollo";
+import "./bootstrap";
 
 //import components
-import PostList from './components/post/PostList';
-import Post from './components/post/Post'
+import PostList from "./components/post/PostList";
+import Post from "./components/post/Post";
 import TopicPostList from "./components/post/TopicPostList";
+import AuthorPostList from "./components/post/AuthorPostList";
 window.Vue = Vue;
 Vue.use(VueRouter);
 
-const routes = [
-    {
-        path : '/',
-        name: 'index',
-        component : PostList
+const routes = [{
+        path: "/",
+        name: "index",
+        component: PostList
     },
     {
-        path : '/post/:id',
-        name : 'post',
-        component : Post
+        path: "/post/:id",
+        name: "post",
+        component: Post
     },
     {
-        path : '/topic/:slug',
-        name : 'topic',
-        component : TopicPostList
+        path: "/topic/:slug",
+        name: "topic",
+        component: TopicPostList
+    },
+    {
+        path: "/authors/:id",
+        name: "authoe",
+        component: AuthorPostList
     }
 ];
 
 Vue.use(VueApollo);
 const apolloClient = new ApolloClient({
     // You should use an absolute URL here
-    uri: 'http://127.0.0.1:8000/graphql'
+    uri: "http://127.0.0.1:8000/graphql"
 });
 
 const apolloProvider = new VueApollo({
-    defaultClient: apolloClient,
+    defaultClient: apolloClient
 });
 
 const router = new VueRouter({
-    mode : 'history',
+    mode: "history",
     routes
 });
 const app = new Vue({
-    el: '#app',
+    el: "#app",
     apolloProvider,
     router
 });
